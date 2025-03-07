371564811a326e339cd95b7d8bbb1f0393484c85
Jose Luis Rojas Severino <jorojas@DCTIC.GC>
2025-03-07 08:55:55 -0400

    Creando el formulario de TableCredentials

--- src/components/Tables/FormTableCredentials.tsx
+++ src/components/Tables/FormTableCredentials.tsx
@@ -8,7 +8,7 @@ return (<>
 
     <div className="row mb-3">
         <div className="col-sm-5 m-1">
-            <label htmlFor="userName"></label>
+            <label htmlFor="userName" className={`${textColorPrimary}`}>User Name</label>
             <input className={`bg-dark form-control ${textColorPrimary}`} placeholder="Usuario" type="text" name="" />
         </div>
         <div className="col-sm-5 m-1">
