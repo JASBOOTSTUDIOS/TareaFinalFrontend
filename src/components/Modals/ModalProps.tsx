e6e51848f647f6cedbd2e455e4865a60bc9fc91b
Jose Luis Rojas Severino <jorojas@DCTIC.GC>
2025-03-10 05:58:30 -0400

    windows Modal addered

---
+++ src/components/Modals/ModalProps.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface ModalProps {
+  show: boolean;
+  handleClose: () => void;
+  title: string;
+  children: React.ReactNode;
+}
+
+export const ModalBootstrap: React.FC<ModalProps> = ({ show, handleClose, title, children }) => {
+  return (
+    <div className={`modal fade ${show ? "show d-block" : "d-none"}`} tabIndex={-1} role="dialog">
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">{title}</h5>
+            <button type="button" className="btn-close" onClick={handleClose}></button>
+          </div>
+          <div className="modal-body">{children}</div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" onClick={handleClose}>
+              Cerrar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
--- src/pages/dashboard/routesDashboard/UsuaiosDashboard.tsx
+++ src/pages/dashboard/routesDashboard/UsuaiosDashboard.tsx
@@ -1,13 +1,26 @@
 import { textColorPrimary } from "../../../components/themesAndColors/TemesAndColors";
 import { CredentialsTable } from "../../../components/Tables/CredentialsTable";
+import { ModalBootstrap } from "../../../components/Modals/ModalProps";
+import { useState } from "react";
 
-
-export default function UsuariosDashboard(){
-    return (<>
-       
-            <div className="row" >
-                    <h1 className={`${textColorPrimary}`}>Tabla de Usuarios</h1>
-                    <CredentialsTable/>
-            </div>  
-    </>)
+export default function UsuariosDashboard() {
+  const [showModal, useShowModal] = useState(false);
+  return (
+    <>
+      <div className="row">
+        <button className="btn btn-info" onClick={() => useShowModal(true)}>
+          Abrir Modal
+        </button>
+        <ModalBootstrap
+          show={showModal}
+          handleClose={() => useShowModal(false)}
+          title="Modal de Prueva"
+        >
+          <h1 className={`${textColorPrimary}`}>Mi Modal Funciona</h1>
+        </ModalBootstrap>
+        <h1 className={`${textColorPrimary}`}>Tabla de Usuarios</h1>
+        <CredentialsTable />
+      </div>
+    </>
+  );
 }
